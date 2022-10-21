ddc7d228194339728c08e5cbbd190c47b442897b
Uminder1313 <umindersingh56@gmail.com>
2022-10-21 22:41:33 +0530

    editfiles

---
+++ Adhyayanmantra/Content/assets/js/Custom.js
@@ -0,0 +1,10 @@
+﻿$('.open-menus').on('click', function (e) {
+    alert(1);
+    console.log("abcs")
+    e.preventDefault();
+    $('.sidebar').addClass('active');
+    $('.overlay').addClass('active');
+    // close opened sub-menus
+    $('.collapse.show').toggleClass('show');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+});
